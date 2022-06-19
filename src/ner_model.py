714235df7f8cf711f16aba27bb68fcedb251dc14
kevinfreire <kfreirea95@gmail.com>
2022-06-19 02:07:13 -0400

    ner model with hugging face pre trained model

---
+++ src/ner_model.py
@@ -0,0 +1,11 @@
+from transformers import BertTokenizer, TFBertForTokenClassification
+from transformers import pipeline
+
+tokenizer = BertTokenizer.from_pretrained("dslim/bert-base-NER")
+model = TFBertForTokenClassification.from_pretrained("dslim/bert-base-NER")
+
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+example = "My name is Kevin , I live at 164 Shoreham Dr. and I work for Apple"
+
+ner_results = nlp(example)
+print(ner_results)
