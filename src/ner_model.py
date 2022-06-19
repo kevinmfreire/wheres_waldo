a5fc9cfa7751af059ea3692b24669bfaf7cd4441
kevinfreire <kfreirea95@gmail.com>
2022-06-19 02:10:50 -0400

    changed example text

--- src/ner_model.py
+++ src/ner_model.py
@@ -5,7 +5,7 @@ tokenizer = BertTokenizer.from_pretrained("dslim/bert-base-NER")
 model = TFBertForTokenClassification.from_pretrained("dslim/bert-base-NER")
 
 nlp = pipeline("ner", model=model, tokenizer=tokenizer)
-example = "My name is Kevin , I live at 164 Shoreham Dr. and I work for Apple"
+example = "My name is Kevin , I live in Canada and I work for Apple"
 
 ner_results = nlp(example)
 print(ner_results)
