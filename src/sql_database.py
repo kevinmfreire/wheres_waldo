0578ac18a673328b83ff8a91dc6fce60996f523c
kevinfreire <kfreirea95@gmail.com>
2022-06-20 23:09:15 -0400

    sql database

--- src/sql_database.py
+++ src/sql_database.py
@@ -4,10 +4,24 @@ import pandas as pd
 from ner_model import model, get_unique_results
 from ws_nbc import web_scrape
 
+'''
+This script is only optimized to handle one news article.
+User Inputs: 
+                article url = Copy and paste an NBC news article URL
+                sql query:
+                    select * from article
+                    select NAME from article
+                    select ORGANIZATION from article
+                    select LOCATION from article
+
+outputs:
+                result query
+'''
+
 def create_sql_server():
     conn = sqlite3.connect('ner_database')
     c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS articles (NAME, ORGANIZATION, LOCATION)')
+    c.execute('CREATE TABLE IF NOT EXISTS article (NAME, ORGANIZATION, LOCATION)')
     conn.commit()
     return conn, c
 
@@ -20,8 +34,9 @@ def dict_to_df(ner_unique):
     return pd.DataFrame(dict([(k,pd.Series(v)) for k,v in ner_unique.items()]))
 
 if __name__ == '__main__':
-    url = 'https://www.nbcnews.com/'
-    article_url = 'https://www.nbcnews.com/politics/biden-says-considering-gas-tax-holiday-rcna34419'
+    # article_url = 'https://www.nbcnews.com/politics/biden-says-considering-gas-tax-holiday-rcna34419'
+
+    article_url = input("Place and NBC News article url: ")
 
     spacy_ner = model()
     nbc_article = web_scrape(article_url)
@@ -34,7 +49,7 @@ if __name__ == '__main__':
 
     conn, c = create_sql_server()
 
-    df.to_sql('articles', conn, if_exists='replace', index = False)
+    df.to_sql('article', conn, if_exists='replace', index = False)
 
     sql_input = ''
     while sql_input != 'quit':
