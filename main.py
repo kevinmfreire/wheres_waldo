41cf0cd1ecd18182b58a928c4c164cf69d87fdc0
kevinfreire <kfreirea95@gmail.com>
2022-06-21 00:51:24 -0400

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import sqlite3
 import pandas as pd
 import sys
+import argparse
 sys.path.append('./src/')
 import ws_nbc, ner_model
 
 '''
-This script is only optimized to handle one news article from NBC News.
-User Inputs: 
-                article url = Copy and paste an NBC news article URL
-                sql query:
-                    select * from article
-                    select NAME from article
-                    select ORGANIZATION from article
-                    select LOCATION from article
+This script has been optimized to handle one or more news article from NBC News.
+User Arguments:
+                --multi_article (Default = False)
+                --num_articles (if multi_article is set to True you can place the amount of articles you want to extract)
+
+User Inputs:
+                Article URL if multi_article is False
 
 outputs:
                 result query
@@ -26,8 +26,7 @@ def create_sql_server():
     return conn, c
 
 def ner_to_df(article):
-    dict1 = article['Article Content'][0]
-    df = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in dict1.items()]))
+    df = pd.DataFrame(dict([(k,pd.Series(v)) for k,v in article.items()]), dtype=object)
     return df
 
 def dict_to_df(ner_unique):
@@ -35,15 +34,29 @@ def dict_to_df(ner_unique):
 
 if __name__ == '__main__':
 
-    article_url = input("Input NBC News article url: ")
+    parser= argparse.ArgumentParser()
+    parser.add_argument('--multi_article', type=bool, default=False)
+    parser.add_argument('--num_articles', type=int, default=5)
+    args = parser.parse_args()
 
     spacy_ner = ner_model.model()
-    nbc_article = ws_nbc.web_scrape(article_url)
 
-    article = nbc_article.scrape_news_article()
-    model_out = spacy_ner.ner(article.get('article content'))
-    ner_unique = ner_model.get_unique_results(model_out)
-    df = dict_to_df(ner_unique)
+    if args.multi_article:
+        nbc_news = 'https://www.nbcnews.com/'
+        nbc_article = ws_nbc.web_scrape(nbc_news)
+        article = nbc_article.scrape_N_articles(num_articles=args.num_articles)
+        model_out = spacy_ner.get_ner_for_all(article)
+        df = pd.DataFrame()
+        for i in range(0,len(model_out)):
+            article_df = ner_to_df(model_out['article content'][i])
+            df = pd.concat([df, article_df])
+    else:
+        article_url = input("Input NBC News article url: ")
+        nbc_article = ws_nbc.web_scrape(article_url)
+        article = nbc_article.scrape_news_article()
+        model_out = spacy_ner.ner(article.get('article content'))
+        ner_unique = ner_model.get_unique_results(model_out)
+        df = dict_to_df(ner_unique)
 
     conn, c = create_sql_server()
 
