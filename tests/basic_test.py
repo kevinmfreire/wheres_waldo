c4281c7b8a75fa4cfe9d8c4f48d003a2167243e9
kevinfreire <kfreirea95@gmail.com>
2022-06-20 22:45:51 -0400

    modifications

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -14,7 +14,19 @@ class Testing(unittest.TestCase):
         ner_results = model.ner(test)
         b = ner_model.get_unique_results(ner_results)
         self.assertEqual(a,b)
-        
+
+    def test_1_web_scraper(self):
+        url = 'https://www.nbcnews.com/politics/biden-says-considering-gas-tax-holiday-rcna34419'
+        scraper = ws_nbc.web_scrape(url)
+        # content = Testing.bs.scrape_news_article
+        content = scraper.scrape_news_article
+        self.assertIsNotNone(content)
+
+    def test_2_n_scraper(self):
+        url = 'https://www.nbcnews.com/'
+        scraper = ws_nbc.web_scrape(url)
+        content = scraper.scrape_N_articles(num_articles=1)
+        self.assertIsNotNone(content)
 
 if __name__ == '__main__':
     unittest.main()
