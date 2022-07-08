e3ad2c7f6c4551c4c2635e76d8bd4e04dcb1f9fb
kevinfreire <kfreirea95@gmail.com>
2022-07-07 22:26:29 -0400

    fixed internal error by adding a results.html script

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask, request, render_template
-import pickle
 from spacy import displacy
 from flaskext.markdown import Markdown
 import sys
+
 sys.path.append("src/")
 
 from src import ner_model, ws_nbc
 
 app = Flask(__name__)
-Markdown(app)
 nlp = ner_model.Model()
+Markdown(app)
 
 @app.route('/')
 def home():
@@ -25,7 +25,7 @@ def predict():
     html = displacy.render(model_out, style='ent')
 
     # return render_template('index.html', prediction_text=f'Your Article Link "{article_url}" has following NER {html}.')
-    return render_template('index.html', prediction_text=html)
+    return render_template('results.html', prediction_text = html)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=8080, debug=True)
